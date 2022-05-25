e6316517db5e75511c803c826564b91837bc0784
Claudio Chavez <97968076+START9INE@users.noreply.github.com>
2022-05-24 20:14:08 -0400

    Update evenorodd.js

---
+++ 8thkyu/evenorodd.js
@@ -0,0 +1,12 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function even_or_odd(number) {
+    //  if(number % 2 == 0){
+    //    return 'Even';
+    //  }else{
+    //    return 'Odd'
+    //  }
+      
+// OR as a ternary operator below!
+
+      return (number % 2 == 0 ? 'Even' : 'Odd' )
+    }
