f82dd210ab03f6a755ac5756e522b87f50fc1415
START9INE <claudchavez1118@gmail.com>
2022-09-22 15:18:24 -0400

    New Array Method Kata

---
+++ 8thkyu/lostwithoutamap.js
@@ -0,0 +1,17 @@
+Description:
+
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+
+//Simple solution
+// Called map with argument num
+// This simply iterates through array and multiplies each element by 2 and creates new array.
+
+function maps(x){
+    return x.map((num) => num * 2);
+    }
+
