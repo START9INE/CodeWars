15f69310e7c030cd4fbf6da9194ffe221754add3
START9INE <claudchavez1118@gmail.com>
2023-02-13 13:56:16 -0500

    New 8thkyu Kata

---
+++ 8thkyu/mathoperators.js
@@ -0,0 +1,45 @@
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+// Examples(Operator, value1, value2) --> output
+
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+//====================================================>
+//Considered bad practice
+//in real world scenarios eval() poses a security risk
+//bad actors can take advantage an run malicious code
+//DO NOT USE
+//=====================================================>
+
+function basicOp(operation, value1, value2) {
+	let str = value1 + operation + value2;
+	return eval(str);
+}
+
+//=======================================>
+//The below is considered best practice
+//simple switch case
+//we check the value of operation parameters
+//and perform actions based on its value
+//Can also be done with conditionals
+//=======================================>
+
+function basicOp(operation, value1, value2) {
+	switch (operation) {
+		case "+":
+			return value1 + value2;
+		case "-":
+			return value1 - value2;
+		case "*":
+			return value1 * value2;
+		case "/":
+			return value1 / value2;
+		default:
+			return 0;
+	}
+}
